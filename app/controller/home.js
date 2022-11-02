caf56d551a64f12324dc193aa3a09343e11bede1
Even <nodeweb@163.com>
2022-11-02 16:09:59 +0800

    修改文件配置

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,10 +1,8 @@
 'use strict';
 
 const Controller = require('egg').Controller;
-const captcha = require('nodejs-captcha');
 class HomeController extends Controller {
   async index() {
-    const ret = captcha();
     ret.width=115;
     ret.height=38;
     // await this.ctx.render('index.html', { message: 'Hello, world!' });
